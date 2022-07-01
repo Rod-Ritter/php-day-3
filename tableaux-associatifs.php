400803ade7744d3f353f3daf7a7173eceb06be7a
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-07-01 10:05:24 +0200

    affiche nom prenom age en json

--- tableaux-associatifs.php
+++ tableaux-associatifs.php
@@ -16,6 +16,22 @@ $peoples = [
     ]
     ];
 
-    debug($peoples);
+    // debug($peoples);
 
-    echo "le nom de Sara est "  .$peoples [1]['nom'];
+    // echo "le nom de Sara est "  .$peoples [1]['nom'];
+
+    // $json_peoples = json_encode($peoples);
+
+    // debug($json_peoples);
+
+    $json_peoples = '{
+    "nom" : "Nooravi",
+    "prenom" : "Amir",
+    "age" : 41
+    
+    }';
+
+    debug($json_peoples);
+
+
+   
