60830a23eac7005ecd26a04207caa7b862bc6f79
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-07-01 10:12:42 +0200

    ajout de profils dans objet json

--- tableaux-associatifs.php
+++ tableaux-associatifs.php
@@ -2,36 +2,42 @@
 
 $peoples = [
     [
-    'nom' => 'Nooravi',
-    'prenom' => 'Amir',
-    'email' => 'a@nooravi.com'
+        'nom' => 'Nooravi',
+        'prenom' => 'Amir',
+        'email' => 'a@nooravi.com'
     ],
 
-    
+
     [
-    'nom' => 'Mousfi',
-    'prenom' => 'Sara',
-    'email' => 's@mousfi.com',
+        'nom' => 'Mousfi',
+        'prenom' => 'Sara',
+        'email' => 's@mousfi.com',
 
     ]
-    ];
+];
 
-    // debug($peoples);
+// debug($peoples);
 
-    // echo "le nom de Sara est "  .$peoples [1]['nom'];
+// echo "le nom de Sara est "  .$peoples [1]['nom'];
 
-    // $json_peoples = json_encode($peoples);
+// $json_peoples = json_encode($peoples);
 
-    // debug($json_peoples);
+// debug($json_peoples);
 
-    $json_peoples = '{
+$json_peoples = '[{
     "nom" : "Nooravi",
     "prenom" : "Amir",
     "age" : 41
     
-    }';
+    },{
+    "nom" : "Mousfi",
+    "prenom" : "Sara",
+    "age" : 31
+        
+    }]';
 
-    debug($json_peoples);
+debug($json_peoples);
 
+$peoples_array_php = json_decode($json_peoples, true);
 
-   
+debug($peoples_array_php);
