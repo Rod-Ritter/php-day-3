c63945c3e4bb434e786971c43af3adc705c6f059
Rod-Ritter <rodolphe.ritter@gmail.com>
2022-07-01 09:58:07 +0200

    tableau rempli avec differentes valeurs

---
+++ tableaux-associatifs.php
@@ -0,0 +1,21 @@
+<?php
+
+$peoples = [
+    [
+    'nom' => 'Nooravi',
+    'prenom' => 'Amir',
+    'email' => 'a@nooravi.com'
+    ],
+
+    
+    [
+    'nom' => 'Mousfi',
+    'prenom' => 'Sara',
+    'email' => 's@mousfi.com',
+
+    ]
+    ];
+
+    debug($peoples);
+
+    // echo "le nom de Sara est $peoples [1],['nom]";
